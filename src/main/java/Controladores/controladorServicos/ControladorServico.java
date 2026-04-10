1d2f050267533dd0b5d5216d181341f08c73296c
Lucas Eduardo <lucaszarzabarros@gmail.com>
2026-04-10 20:07:34 -0300

    feat: criação do controlador de serviços

--- src/main/java/Controladores/controladorServicos/ControladorServico.java
+++ src/main/java/Controladores/controladorServicos/ControladorServico.java
@@ -1,5 +1,44 @@
 package Controladores.controladorServicos;
 
-public class ControladorServico {
+import entity.Servico;
+import service.servico.AtualizarServico;
+import service.servico.CadastroServico;
+import service.servico.ServicoService;
+import util.LimpaTerminal;
+import util.VerificaEntradas;
+import view.MenuView;
 
+public class ControladorServico {
+    private ServicoService servicoService;
+
+    public ControladorServico(ServicoService servicoService) {
+        this.servicoService = servicoService;
+    }
+
+    public void menuServico() {
+        LimpaTerminal.limparTela();
+        int opcao = -1;
+
+        while (opcao != 0) {
+            MenuView.exibirMenuServicos();
+            opcao = VerificaEntradas.lerInteiro("Escolha uma opção: ");
+            switch (opcao) {
+                case 1:
+                    Servico servicoCadastrar = CadastroServico.cadastrarServico();
+                    if (servicoCadastrar != null) {
+                        servicoService.cadastrar(servicoCadastrar);
+                    }
+                    break;
+                case 2:
+                    servicoService.listarTodos();
+                    break;
+                case 3:
+                    Servico servicoAtualizado = AtualizarServico.atualizarServico(servicoService);
+                    if (servicoAtualizado != null) {
+                        servicoService.atualizar(servicoAtualizado);
+                    }
+                    break;
+            }
+        }
+    }
 }
