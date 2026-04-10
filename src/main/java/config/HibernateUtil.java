da2061630b23e03ee52515994db1b0bcff5e7580
eduardoflorenciano <eduardoflorencianoo@gmail.com>
2026-04-10 18:16:18 -0300

    feat: adicionar as funcionalidades das classes do pacote 'config'

--- src/main/java/config/HibernateUtil.java
+++ src/main/java/config/HibernateUtil.java
@@ -1,5 +1,21 @@
 package config;
 
-public class HibernateUtil {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
 }
