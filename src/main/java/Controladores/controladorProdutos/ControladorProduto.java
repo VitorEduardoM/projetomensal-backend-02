c32c706196c122d0d82dd3c561432ac61a8d65c2
Lucas Eduardo <lucaszarzabarros@gmail.com>
2026-04-10 20:04:50 -0300

    feat: criação do controlador de produtos

--- src/main/java/Controladores/controladorProdutos/ControladorProduto.java
+++ src/main/java/Controladores/controladorProdutos/ControladorProduto.java
@@ -1,5 +1,40 @@
 package Controladores.controladorProdutos;
 
-public class ControladorProduto {
+import entity.Produto;
+import service.produto.AtualizarProduto;
+import service.produto.CadastroDeProduto;
+import service.produto.ProdutoService;
+import util.LimpaTerminal;
+import util.VerificaEntradas;
+import view.MenuView;
 
+public class ControladorProduto {
+    private ProdutoService produtoService = new ProdutoService();
+
+    public void menuProdutos() {
+        LimpaTerminal.limparTela();
+        int opcao = -1;
+
+        while (opcao != 0) {
+            MenuView.exibirMenuProdutos();
+            opcao = VerificaEntradas.lerInteiro("Escolha uma opção: ");
+            switch (opcao) {
+                case 1:
+                    Produto produtoCadastrar = CadastroDeProduto.cadastrarProduto();
+                    if (produtoCadastrar != null) {
+                        produtoService.cadastrar(produtoCadastrar);
+                    }
+                    break;
+                case 2:
+                    produtoService.listarTodos();
+                    break;
+                case 3:
+                    Produto produtoAtualizado = AtualizarProduto.atualizarProduto(produtoService);
+                    if (produtoAtualizado != null) {
+                        produtoService.atualizar(produtoAtualizado);
+                    }
+                    break;
+            }
+        }
+    }
 }
