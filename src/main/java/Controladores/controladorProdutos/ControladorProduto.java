99cb2882001cddc0dd971d31c8a21b54b8753618
Lucas Eduardo <lucaszarzabarros@gmail.com>
2026-04-10 20:05:53 -0300

    feat: finalizando a criação do controlador de pprodutos

--- src/main/java/Controladores/controladorProdutos/ControladorProduto.java
+++ src/main/java/Controladores/controladorProdutos/ControladorProduto.java
@@ -34,6 +34,26 @@ public class ControladorProduto {
                         produtoService.atualizar(produtoAtualizado);
                     }
                     break;
+                case 4:
+                    System.out.println("\n========== REMOVER PRODUTO ==========");
+                    produtoService.listarTodos();
+                    Long idRemover = VerificaEntradas.lerLong("ID do produto a remover: ");
+                    produtoService.remover(idRemover);
+                    break;
+                case 5:
+                    String buscarNome = VerificaEntradas.lerTexto("Digite o nome para buscar: ");
+                    produtoService.buscarPorNome(buscarNome);
+                    break;
+                case 6:
+                    String buscarCategoria = VerificaEntradas.lerTexto("Digite a categoria para buscar: ");
+                    produtoService.buscarPorCategoria(buscarCategoria);
+                    break;
+                case 7:
+                    produtoService.exibirRelatorio();
+                    break;
+                case 0:
+                    System.out.println("Voltando ao menu principal");
+                    break;
             }
         }
     }
